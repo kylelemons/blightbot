58abff7ab09fe822abcf6888d4a5f19781619005
Kyle Lemons <kyle@kylelemons.net>
2012-02-24 23:27:19 -0800

    gofmt

--- gonuts/3pkg.go
+++ gonuts/3pkg.go
@@ -28,14 +28,14 @@ func tpdoc(src *commander.Source, resp *commander.Response, cmd string, args []s
 	pkg := args[0]
 	for _, ch := range pkg {
 		switch ch {
-			case 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z':
-			case 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z':
-			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-			case '-', '.', '_', '/':
-			default:
-				resp.Private()
-				resp.Printf("Hmm, that doesn't look like a package name...")
-				return
+		case 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z':
+		case 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z':
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		case '-', '.', '_', '/':
+		default:
+			resp.Private()
+			resp.Printf("Hmm, that doesn't look like a package name...")
+			return
 		}
 	}
 
