bd673cd982b89cf4349f17723586dd7970806733
Kyle Lemons <kyle@kylelemons.net>
2013-01-07 01:09:49 -0800

    Fix 3pkg

--- gonuts/3pkg.go
+++ gonuts/3pkg.go
@@ -7,8 +7,8 @@ import (
 	"github.com/kylelemons/blightbot/commander"
 )
 
-var ThirdPartyHost = "gopkgdoc.appspot.com"
-var ThirdPartyIndex = "http://gopkgdoc.appspot.com/index"
+var ThirdPartyHost = "godoc.org"
+var ThirdPartyIndex = "http://godoc.org/-/index"
 
 /* TODO cache?
 var tpCacheLock sync.Mutex
@@ -48,7 +48,7 @@ func tpdoc(src *commander.Source, resp *commander.Response, cmd string, args []s
 	// If the query did not redirect, then GoPkgDoc could not find the package.
 	if err != nil || r.StatusCode != http.StatusOK || r.Request.URL.Path == "/" {
 		resp.Public()
-		resp.Printf("Hmm, I can't find %q.  You can look for it on %s .", pkg, ThirdPartyIndex)
+		resp.Printf("Hmm, I can't find %q.  You can look for it on %s", pkg, ThirdPartyIndex)
 		return
 	}
 	resp.Public()
