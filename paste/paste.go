734ff4a90adf00f1c5b9a776329c4d3fbca8919b
Kyle Lemons <kyle@kylelemons.net>
2013-01-07 01:10:15 -0800

    update paste functionality

--- paste/paste.go
+++ paste/paste.go
@@ -1,103 +1,101 @@
 package paste
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"flag"
-	"github.com/kylelemons/blightbot/bot"
-	"github.com/kylelemons/blightbot/commander"
-	"github.com/kylelemons/go-rpcgen/webrpc"
-	"github.com/kylelemons/gopaste/proto"
+	"fmt"
+	"io"
 	"log"
-	"net/url"
 	"strings"
 	"sync"
-	"time"
+
+	"github.com/kylelemons/blightbot/bot"
+	"github.com/kylelemons/blightbot/commander"
+	"github.com/kylelemons/gopaste/subscribe"
 )
 
 var (
 	chans = flag.String("paste-chans", "", "Channels to send paste notifications on")
-	psrvurl = flag.String("paste-server", "http://paste.kylelemons.net:4114/", "Server to query for pastes")
 )
 
 func nopaste(s *commander.Source, r *commander.Response, cmd string, args []string) {
 	r.Public()
-	r.Printf("If you need to paste more than 3 lines, use gopaste: https://github.com/kylelemons/gopaste/")
+	r.Printf("If you need to paste more than 3 lines, use gp: go get github.com/kylelemons/gopaste/gp")
 }
 
 var NoPaste = commander.Cmd("nopaste", nopaste).Help(`Print a message pointing to the gopaste tool`)
 
-var servers = map[string]*bot.Server{}
-var servlock sync.Mutex
+var servers = struct {
+	sync.Mutex
+	m map[string]*bot.Server
+}{m: map[string]*bot.Server{}}
 
 func addServer(event string, serv *bot.Server, msg *bot.Message) {
-	servlock.Lock()
-	defer servlock.Unlock()
+	servers.Lock()
+	defer servers.Unlock()
 
-	servers[serv.Name()] = serv
+	servers.m[serv.Name()] = serv
 }
 
 func delServer(event string, serv *bot.Server, msg *bot.Message) {
-	servlock.Lock()
-	defer servlock.Unlock()
+	servers.Lock()
+	defer servers.Unlock()
 
-	delete(servers, serv.Name())
+	delete(servers.m, serv.Name())
 }
 
 func Register(b *bot.Bot) {
 	if len(*chans) == 0 {
 		log.Printf("skipping paste init: no channels to notify")
+		return
 	}
 
 	b.OnConnect(addServer)
 	b.OnDisconnect(delServer)
 
-	url, err := url.Parse(*psrvurl)
-	if err != nil {
-		log.Fatalf("paste: bad url %q: %s", *psrvurl, err)
-	}
-
-	psrv := proto.NewGoPasteWebClient(webrpc.ProtoBuf, url)
-	go waitloop(psrv)
+	go pasteloop()
 }
 
-func waitloop(psrv proto.GoPaste) {
+func pasteloop() {
 	chans := strings.Split(*chans, ",")
-	in, out := proto.Empty{}, proto.Posted{}
 
-	var (
-		errdelay = 100*time.Millisecond
-		startdelay = 100*time.Millisecond
-		maxdelay = 10*time.Minute
-	)
-	for {
-		err := psrv.Next(&in, &out)
-		if err != nil {
-			log.Printf("paste: next URL: %s", err)
-			time.Sleep(errdelay)
-			errdelay *= 2
-			if errdelay > maxdelay {
-				errdelay = maxdelay
+	random := make([]byte, 16)
+	if _, err := io.ReadFull(rand.Reader, random); err != nil {
+		panic("not enough entropy")
+	}
+	clientID := base64.URLEncoding.EncodeToString(random)
+
+	send := func(url string) {
+		servers.Lock()
+		defer servers.Unlock()
+
+		msg := &bot.Message{
+			Command: bot.CMD_PRIVMSG,
+			Args: []string{
+				"",
+				fmt.Sprintf("pasted: %s", url),
+			},
+		}
+		for sname, srv := range servers.m {
+			for _, channel := range chans {
+				log.Printf("Writing to %s on %s", channel, sname)
+				msg.Args[0] = channel
+				srv.WriteMessage(msg)
 			}
-			continue
 		}
-		errdelay = startdelay
+	}
 
-		if out.Url == nil {
-			log.Printf("paste: warning: empty URL")
-			continue
-		}
-		
-
-		func(){
-			servlock.Lock()
-			defer servlock.Unlock()
-
-			for _, cname := range chans {
-				msg := bot.NewMessage("", bot.CMD_PRIVMSG, cname, "gopaste: "+ *out.Url)
-				for sname, s := range servers {
-					log.Printf("paste: sending paste to %s on %s", cname, sname)
-					s.WriteMessage(msg)
-				}
+	for {
+		log.Printf("Starting paste subscriber...")
+		urls := make(chan string)
+		go func() {
+			if err := subscribe.Subscribe(clientID, urls); err != nil {
+				log.Printf("subscribe: %s", err)
 			}
 		}()
+		for url := range urls {
+			send(url)
+		}
 	}
 }
